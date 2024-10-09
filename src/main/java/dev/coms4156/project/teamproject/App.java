65b2acf65ff0ea87a8c5696431d0b54a5db26f34
Jason Han <jason.han@columbia.edu>
2024-10-09 17:27:42 -0400

    Install JDBC dependency; add JDBC connection/query example

--- src/main/java/dev/coms4156/project/teamproject/App.java
+++ src/main/java/dev/coms4156/project/teamproject/App.java
@@ -1,21 +1,20 @@
 package dev.coms4156.project.teamproject;
 
 import jakarta.annotation.PreDestroy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Class contains all the startup logic for the application.
- *
- * <p>DO NOT MODIFY ANYTHING BELOW THIS POINT WITH REGARD TO FUNCTIONALITY
- * YOU MAY MAKE STYLE/REFACTOR MODIFICATIONS AS NEEDED
- */
 @SpringBootApplication
 public class App implements CommandLineRunner {
-
     /**
      * The main launcher for the service all it does is make a call to the overridden run method.
      *
@@ -34,6 +33,29 @@ public class App implements CommandLineRunner {
      */
     @Override
     public void run(String[] args) {
+        databaseExperiment();
         System.out.println("Start up");
+    }
+
+    private static void databaseExperiment() {
+        String url = "jdbc:postgresql://34.86.228.87/team_project_database";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "ase_aces");
+
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM employee WHERE salary > 500");
+            while (rs.next()) {
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(1));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
