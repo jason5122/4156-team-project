e3bb3ac4cb6657598cc73f8079033e5fd73f51c3
Ella Kim <kimella@grinnell.edu>
2024-10-10 14:21:51 -0400

    Set a unique donation ID

--- src/main/java/dev/coms4156/project/teamproject/Donation.java
+++ src/main/java/dev/coms4156/project/teamproject/Donation.java
@@ -2,10 +2,10 @@ package dev.coms4156.project.teamproject;
 
 import java.io.Serial;
 import java.time.LocalDateTime;
-import java.util.Random;
+import java.time.format.DateTimeFormatter;
 
 /**
- * Represents a donation
+ * Represents a donation.
  */
 public class Donation {
   @Serial
@@ -14,25 +14,40 @@ public class Donation {
   private int listingId;
   private int quantityPickedUp;
   private LocalDateTime pickUpTime;
-  private final int donationId;
+  private final String donationId;
+
   /**
-   * Creates a new donation object with the given params
+   * Creates a new donation object with the given params.
    *
    * @param accountId Account id of the provider who put up the foodListing
    * @param listingId Id of the food listing referenced by the donation
    * @param pickUpTime Pick up time of the donation
    * @param quantityPickedUp Quantity of the food listing pick up
+   * @param donationId Id of the donation 
    */
-  public Donation(String accountId, int listingId, int quantityPickedUp, LocalDateTime pickUpTime) {
+  public Donation(String accountId, int listingId, int quantityPickedUp, 
+        LocalDateTime pickUpTime, String donationId) {
     this.accountId = accountId;
     this.listingId = listingId;
     this.quantityPickedUp = quantityPickedUp;
     this.pickUpTime = pickUpTime;
-    Random rand = new Random();
-    this.donationId = rand.nextInt(1000);
+    this.donationId = setDonationId(accountId);
   }
 
-  public int getDonationId() {
+  /**
+   * Generates a unique donation ID to use. 
+   *
+   * @return String representing a unique donation from the account.
+   */
+  public static String setDonationId(String accountId) {
+    LocalDateTime current = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+    String timestamp = current.format(formatter);
+
+    return accountId + timestamp;
+  }
+
+  public String getDonationId() {
     return donationId;
   }
 
