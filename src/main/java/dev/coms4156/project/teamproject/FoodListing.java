41a9248de61962953cd92755014db5981e1711a0
Ella Kim <kimella@grinnell.edu>
2024-10-14 04:30:39 -0400

    Checkstyle fixes

--- src/main/java/dev/coms4156/project/teamproject/FoodListing.java
+++ src/main/java/dev/coms4156/project/teamproject/FoodListing.java
@@ -3,7 +3,6 @@ package dev.coms4156.project.teamproject;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
