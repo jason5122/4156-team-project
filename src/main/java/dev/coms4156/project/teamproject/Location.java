bb1971ca1b0022360d67cabe7fb425708a89826a
Jason Han <jason.han@columbia.edu>
2024-10-14 15:51:51 -0400

    Add Location class; add unit tests for Location

---
+++ src/main/java/dev/coms4156/project/teamproject/Location.java
@@ -0,0 +1,34 @@
+package dev.coms4156.project.teamproject;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+    public double latitude;
+    public double longitude;
+    private static final int EARTH_RADIUS_KM = 6378;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Gets the distance between two Locations.
+     */
+    public double distance(Location other) {
+        double dLat = Math.toRadians(latitude - other.latitude);
+        double dLong = Math.toRadians(longitude - other.longitude);
+
+        double dStartLat = Math.toRadians(latitude);
+        double dEndLat = Math.toRadians(other.latitude);
+
+        double a = haversine(dLat) + Math.cos(dStartLat) * Math.cos(dEndLat) * haversine(dLong);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS_KM * c;
+    }
+
+    private static double haversine(double val) {
+        return Math.pow(Math.sin(val / 2), 2);
+    }
+}
