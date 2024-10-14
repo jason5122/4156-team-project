3afbd14b8be8f944639f4a51de3642cac6efcde6
Jason Han <jason.han@columbia.edu>
2024-10-14 19:35:29 -0400

    Fix JPA not creating tables in database

--- src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
+++ src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
@@ -1,9 +1,8 @@
 package dev.coms4156.project.teamproject.repository;
 
+import dev.coms4156.project.teamproject.FoodListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import dev.coms4156.project.teamproject.FoodListing;
 
 @Repository
 public interface FoodListingRepository extends JpaRepository<FoodListing, Integer> {}
