50beac27a99b6045300e46f265b5a0d6affbc812
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-10-17 18:12:28 -0400

    feat: implement /getRequestsForListing

--- src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
+++ src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
@@ -4,6 +4,7 @@ import dev.coms4156.project.teamproject.model.AccountProfile;
 import dev.coms4156.project.teamproject.model.ClientProfile;
 import dev.coms4156.project.teamproject.model.FoodListing;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface FoodListingRepository extends JpaRepository<FoodListing, Integer> {
     public List<FoodListing> findByClient(ClientProfile client);
     public List<FoodListing> findByClientAndAccount(ClientProfile client, AccountProfile account);
+    public Optional<FoodListing> findByClientAndAccountAndListingId(ClientProfile client,
+                                                                    AccountProfile account,
+                                                                    int listingId);
 }
--- src/main/java/dev/coms4156/project/teamproject/repository/FoodRequestRepository.java
+++ src/main/java/dev/coms4156/project/teamproject/repository/FoodRequestRepository.java
@@ -1,5 +1,7 @@
 package dev.coms4156.project.teamproject.repository;
 
+import dev.coms4156.project.teamproject.model.AccountProfile;
+import dev.coms4156.project.teamproject.model.ClientProfile;
 import dev.coms4156.project.teamproject.model.FoodRequest;
 import dev.coms4156.project.teamproject.model.FoodListing;
 import java.util.List;
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FoodRequestRepository extends JpaRepository<FoodRequest, Integer> {
   public List<FoodListing> findByClient_ClientId(int clientId);
+  public List<FoodRequest> findByClientAndFoodListing(ClientProfile client, FoodListing foodListing);
 }
