581f70916dc1a9ce937f569d42d2943450efbc18
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-10-16 20:09:44 -0400

    feat: add JPA derived query method to filter food listings by clientId

--- src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
+++ src/main/java/dev/coms4156/project/teamproject/repository/FoodListingRepository.java
@@ -1,9 +1,12 @@
 package dev.coms4156.project.teamproject.repository;
 
+import dev.coms4156.project.teamproject.model.ClientProfile;
+import dev.coms4156.project.teamproject.model.FoodListing;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import dev.coms4156.project.teamproject.model.FoodListing;
-
 @Repository
-public interface FoodListingRepository extends JpaRepository<FoodListing, Integer> {}
+public interface FoodListingRepository extends JpaRepository<FoodListing, Integer> {
+  public List<FoodListing> findByClient_ClientId(int clientId);
+}
