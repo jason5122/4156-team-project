9d80875230f6c491176a675052637b1f6c53c9a5
Unnati Pulla <unnatipulla@unnatis-mbp-2.mynetworksettings.com>
2024-11-17 23:42:45 -0500

    Data Initialzed at start up for client to query

--- src/main/java/dev/coms4156/project/teamproject/service/DataInitializer.java
+++ src/main/java/dev/coms4156/project/teamproject/service/DataInitializer.java
@@ -41,16 +41,16 @@ public class DataInitializer implements CommandLineRunner {
     accountProfileRepository.save(provider2);
 
     // Create Food Listings for each provider
-    FoodListing listing1 = new FoodListing(null, provider1, "Sandwiches", 20,
+    FoodListing listing1 = new FoodListing(foodDonationClient, provider1, "Sandwiches", 20,
         LocalDateTime.now().plusHours(5), 34.052f, -118.243f);
-    FoodListing listing2 = new FoodListing(null, provider1, "Pasta", 15,
+    FoodListing listing2 = new FoodListing(foodDonationClient, provider1, "Pasta", 15,
         LocalDateTime.now().plusHours(3), 34.052f, -118.243f);
-    FoodListing listing3 = new FoodListing(null, provider1, "Salads", 10,
+    FoodListing listing3 = new FoodListing(foodDonationClient, provider1, "Salads", 10,
         LocalDateTime.now().plusHours(6), 34.052f, -118.243f);
 
-    FoodListing listing4 = new FoodListing(null, provider2, "Fruit Basket", 8,
+    FoodListing listing4 = new FoodListing(foodDonationClient, provider2, "Fruit Basket", 8,
         LocalDateTime.now().plusHours(4), 40.7128f, -74.0060f);
-    FoodListing listing5 = new FoodListing(null, provider2, "Cereal", 12,
+    FoodListing listing5 = new FoodListing(foodDonationClient, provider2, "Cereal", 12,
         LocalDateTime.now().plusHours(2), 40.7128f, -74.0060f);
 
     foodListingRepository.save(listing1);
